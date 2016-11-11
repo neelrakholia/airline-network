c2fa4cd87dae331d76d5b0f7a1a79bd3f06410f9
Noam Habot <noam.habot@gmail.com>
2016-11-11 11:31:46 -0800

    added imports to top of file

--- calculations.py
+++ calculations.py
@@ -1,5 +1,11 @@
 # this script takes in the binary files for each day and performs calculations on them
 
+import os
+import snap
+from numpy import trapz
+import numpy
+import matplotlib.pyplot as plt
+
 directory = os.path.join(os.getcwd(), 'data')
 graphDict = {}
 
@@ -13,7 +19,7 @@ for date in os.listdir(directory):
 # want a function that gets the nodes with the highest betweenness centrality
 # and deletes them from the graph
 # and then computes the size of the largest weakly connected components
-def computeResiliency(graph):
+def computeResiliency(graph, toPlot):
     degreeCentralityDict = {}
     for N in graph.Nodes():
         degreeCentralityDict[N.GetId()] = snap.GetDegreeCentr(graph, NI.GetId())
@@ -26,13 +32,24 @@ def computeResiliency(graph):
         largestNode = max(degreeCentralityDict, key=degreeCentralityDict.get)
         graph.DelNode(largestNode)
 
-    plt.plot(fractionNodesRemoved, fractionInLWCC)
-    plt.xlabel('Fraction Nodes Removed')
-    plt.ylabel('Fraciton of Nodes still in LWCC')
-    plt.title('Nodes in LWCC vs. Removal of Nodes')
-    plt.show()
+    y = numpy.array(fractionInLWCC)
+
+
+    # Compute the area using the composite trapezoidal rule.
+    resiliencyIndex = trapz(y, dx=float(1/graph.GetNodes())) # area under curve
+
+    if toPlot:
+        plt.plot(fractionNodesRemoved, fractionInLWCC)
+        plt.xlabel('Fraction Nodes Removed')
+        plt.ylabel('Fraciton of Nodes still in LWCC')
+        plt.title('Nodes in LWCC vs. Removal of Nodes')
+        plt.show()
+
+    return resiliencyIndex
+
+
 
 
 
 #for date, graphObject in graphDict.iteritems():
-#    computeResiliency(graphObject)
+#    computeResiliency(graphObject, FALSE)

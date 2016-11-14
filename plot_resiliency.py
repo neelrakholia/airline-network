70a60124949d7bf4fa24e3e23ad3c64b90dc790e
Poorvi <poorvibhargava@DN0a22b8ac.SUNet>
2016-11-14 15:33:47 -0800

    plotted betweenness

---
+++ plot_resiliency.py
@@ -0,0 +1,14 @@
+import sys
+import matplotlib.pyplot as plt
+import csv
+
+with open(sys.argv[1], 'rb') as f:
+    reader = csv.reader(f)
+    data = list(reader)[0]
+
+
+plt.plot(range(1, len(data)+1), data, marker='o', markersize = '3')
+plt.xlabel('Day of Year')
+plt.ylabel('Resiliency Index')
+plt.title('Resiliency Index by Day of Year (Betweenness Centrality)')
+plt.show()
